e98773ce8b6060a42b1376a91f90354bd65b659f
Ahmed <a.8salah@gmmail.com>
2026-03-23 18:58:52 +0200

    fix: add placeholders for supabase to pass build

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,20 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-let supabase: ReturnType<typeof createClient>
-
-if (!globalThis.supabase) {
-  globalThis.supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true,
-    },
-  })
-}
-
-supabase = globalThis.supabase
-
-export { supabase }
+export const supabase = createClient(supabaseUrl, supabaseKey)
